8bea8aaf1a1329757d05cf67acfed2c4ebf3e378
david.varga <tewwe@sch.hu>
2018-02-14 09:46:39 +0100

    Remove auth logs

--- entry.sh
+++ entry.sh
@@ -65,9 +65,6 @@ if [ $# == 0  ] || [ "${1#-}" != "$1" ]             ##### in case of no argument
       egrep -iv "${optionsgrep%|}" /usr/local/etc/tinyproxy/tinyproxy.conf   > /tmp/tinyproxy.conf
       cat                          /tmp/tinyproxy.add   >> /tmp/tinyproxy.conf
       chown -R nobody:nogroup /tmp
-      echo "Config file:"
-      cat /tmp/tinyproxy.conf
-      echo "Auth env: $(AUTH_USER) $(AUTH_PASS)"
       set -- tinyproxy -d -c /tmp/tinyproxy.conf
    fi
 
